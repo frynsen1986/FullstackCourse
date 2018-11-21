dc095046eb1418784158075ec7d7e01b48361fb9
Stefan Hartung Jørgensen <frynsen1986@gmail.com>
2018-11-21 10:53:56 +0100

    Stuff

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -4,25 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FirstConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HelloWorld(args);
+
+            PlayingWithPerson();
+            PlayingWithAdd();
+
+            
+
+        }
+
+     
+
+        static void HelloWorld(string[] args)
+        {
             Console.WriteLine("Hello world");
             char input = Console.ReadKey(true).KeyChar;
-            Console.WriteLine(String.Format("Du har trykket på: {0} ({1})", input, (int)input));
+            Console.WriteLine("Du har trykket på: {0} ({1})", input, (int)input);
 
-            Console.WriteLine(String.Format("Der er modaget {0} argumenter:",args.Length));
+            Console.WriteLine("Der er modaget {0} argumenter:", args.Length);
             for (int idx = 0; idx < args.Length; idx++)
             {
-                Console.WriteLine(String.Format("Arg nummer {0} er {1}", idx, args[idx]));
+                Console.WriteLine("Arg nummer {0} er {1}", idx, args[idx]);
             }
+        }
 
+        static void PlayingWithPerson()
+        {
             var minPerson = new FirstConsoleApp.Persons.Person();
 
             Console.WriteLine("Alder på person: " + minPerson.Age);
         }
+        
+        static void PlayingWithAdd()
+        {
+            int a = 4, b = 2;
+            Console.WriteLine("a = {0} b = {1}", a, b);
+            int c = Add(ref a, ref b);
+            Console.WriteLine("a = {0} b = {1} c = a + b = {2}", a, b, c);
+        }
+
+        static int Add(ref int x, ref int y)
+        {
+            x = 8;
+            return x + y;
+        }
     }
+
+
 }
---
+++ FourthCoffee.MethodTestHarness/Utilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCoffee.MethodTestHarness
+{
+    public class Utilities
+    {
+        public Utilities() 
+        {
+            // TODO: 02: Invoke the Initialize method.
+            bool isInitialized = Initialize();
+        }
+
+        // TODO: 01: Define the Initialize method.
+        bool Initialize()
+        {
+            string path = GetApplicationPath();
+            return !string.IsNullOrEmpty(path);
+        }
+
+
+        #region Helper methods
+
+        string GetApplicationPath()
+        {
+            return Assembly.GetExecutingAssembly().Location;
+        }
+
+        #endregion
+    }
+}
