a3329a8860caa3f8f3935d254c6eed2a4cd1e5b6
Stefan Hartung Jørgensen <frynsen1986@gmail.com>
2018-11-22 13:04:47 +0100

    Tilføjet forskellige ting til at test og øve C#

---
+++ FirstConsoleApp/Drinks/Coffee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp.Drinks
+{
+    public struct Coffee
+    {
+        private int strength;
+        public int Strength
+        {
+            get { return strength; }
+            set {
+                if (strength <= 0)
+                    strength = value;
+            }
+        }
+        private string bean;
+        public string Bean
+        {
+            get => bean;    // Lambda for at returnere bean.
+            set
+            {
+                if (string.IsNullOrEmpty(bean))
+                    bean = value;
+            }
+
+        }
+        private string countryOfOrigin;
+        public string CountryOfOrigin
+        {
+            get => countryOfOrigin;    // Lambda for at returnere bean.
+            set
+            {
+                if (string.IsNullOrEmpty(countryOfOrigin))
+                    countryOfOrigin = value;
+            }
+
+        }
+
+        public Coffee(int strength, string bean, string countryOfOrigin)
+        {
+            this.strength = strength;
+            this.bean = bean;
+            this.countryOfOrigin = countryOfOrigin;
+        }
+    }
+}
--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -11,12 +11,14 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            HelloWorld(args);
+            //HelloWorld(args);
 
-            PlayingWithPerson();
-            PlayingWithAdd();
+            //PlayingWithPerson();
+            //PlayingWithAdd();
 
-            
+            //PlayingWithStructs();
+
+            PlayingWithCollections();
 
         }
 
@@ -24,7 +26,7 @@ namespace FirstConsoleApp
 
         static void HelloWorld(string[] args)
         {
-            Console.WriteLine("Hello world");
+            System.Console.WriteLine("Hello world");
             char input = Console.ReadKey(true).KeyChar;
             Console.WriteLine("Du har trykket på: {0} ({1})", input, (int)input);
 
@@ -46,8 +48,12 @@ namespace FirstConsoleApp
         {
             int a = 4, b = 2;
             Console.WriteLine("a = {0} b = {1}", a, b);
-            int c = Add(ref a, ref b);
+            int c = Add(a, b);
             Console.WriteLine("a = {0} b = {1} c = a + b = {2}", a, b, c);
+
+            Console.WriteLine("a = {0} b = {1}", a, b);
+            int d = Add(ref a, ref b);
+            Console.WriteLine("a = {0} b = {1} c = a + b = {2}", a, b, d);
         }
 
         static int Add(ref int x, ref int y)
@@ -55,6 +61,106 @@ namespace FirstConsoleApp
             x = 8;
             return x + y;
         }
+
+        static int Add(int x, int y)
+        {
+            x = 8;
+            return x + y;
+        }
+
+
+        static void PlayingWithStructs()
+        {
+            var coffee = new Drinks.Coffee(3,"Arabica","Baracuda");
+            coffee.Strength = 4;
+
+            Console.WriteLine(
+                "Country of origin:     {0}\n" +
+                "Bean:                  {1}\n" +
+                "Strength:              {2}\n\n"
+                , coffee.CountryOfOrigin
+                , coffee.Bean
+                , coffee.Strength);
+
+            var defaultCoffee = new Drinks.Coffee();
+            Console.WriteLine(
+                "Country of origin:     {0}\n" +
+                "Bean:                  {1}\n" +
+                "Strength:              {2}"
+                , defaultCoffee.CountryOfOrigin
+                , defaultCoffee.Bean
+                , defaultCoffee.Strength);
+
+            defaultCoffee.Bean = "My favorite";
+            defaultCoffee.CountryOfOrigin = "Fav country";
+            defaultCoffee.Strength = 8;
+            Console.WriteLine(
+                "Country of origin:     {0}\n" +
+                "Bean:                  {1}\n" +
+                "Strength:              {2}"
+                , defaultCoffee.CountryOfOrigin
+                , defaultCoffee.Bean
+                , defaultCoffee.Strength);
+
+            var coffeeTest = new Drinks.Coffee
+            {// Initializer list:
+                Bean = "Ingen",
+                Strength = 3,
+                CountryOfOrigin = "Some place"
+            };
+
+            // Virker ikke idet structed ikke implementerer System.Collections.IEnumerable
+            //var coffeeTest2 = new Drinks.Coffee
+            //{// Initializer list:
+            //    3,
+            //    "Ingen",
+            //    "Some place"
+            //};
+
+        }
+
+        static void PlayingWithCollections()
+        {
+            #region Collections and Linq
+            var prices = new System.Collections.Hashtable();
+
+            prices.Add("Cafe au Lait", 1.99M);
+            prices.Add("Cafe Americano", 1.89M);
+            prices.Add("Cafe Mocha", 2.99M);
+            prices.Add("Espresso", 1.49M);
+            prices.Add("Juice", 1.11M);
+
+            var bargins =
+                from string drink in prices.Keys
+                where (Decimal)prices[drink] < 1.50M
+                orderby prices[drink] ascending
+                select drink;
+
+            var mostExpensive =
+                from string drink in prices.Keys
+                where (Decimal)prices[drink] > 1.50M
+                orderby prices[drink] ascending
+                select drink;
+
+            Console.WriteLine("Printing the bargins:");
+            foreach (var bargin in bargins)
+            {
+                Console.WriteLine(bargin + ": " + prices[bargin]);
+            }
+
+
+            Console.WriteLine("\nPrinting the pricy beverages:");
+            foreach (string pricy in mostExpensive)
+            {
+                Console.WriteLine(pricy + ": " + prices[pricy]);
+            }
+
+            #endregion
+        }
+
+        
+
+
     }
 
 
